92ffa84c0383c508d9efb30a46cb490415e877bc
donn-majidi <donn.majidi@gmail.com>
2026-08-24 01:26:24 +0200

    Update: Bring repo up to date.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import matplotlib.pyplot as plt
 from src.Gaussian_ensembles import gaussian_ensembles_plot
+from src.Wishart_ensembles import wishart_ensembles
 
 fig, (ax1, ax2, ax4) = plt.subplots(1, 3, figsize = (18,12))
 
-gaussian_ensembles_plot(ax1, 8, 1, 50000)
-gaussian_ensembles_plot(ax2, 8, 2, 50000)
-gaussian_ensembles_plot(ax4, 8, 4, 50000)
+gaussian_ensembles_plot(ax1, 20, 1, 1000)
+gaussian_ensembles_plot(ax2, 20, 2, 1000)
+gaussian_ensembles_plot(ax4, 20, 4, 1000)
 
 plt.show()
+
+beta = [1,2,4]
+N = 8
+M = N + 7
+simul_count = 1000
+
+for i in beta:
+    wishart_ensembles(N, M, i, simul_count)
+
